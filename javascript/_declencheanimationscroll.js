d586162741645e96e1df29f2ebf05eef3359a766
QuentinHIL <hillairetq@gmail.com>
2024-01-03 18:32:27 +0100

    maj_janvier_2024

--- javascript/_declencheanimationscroll.js
+++ javascript/_declencheanimationscroll.js
@@ -69,6 +69,76 @@ const observerquatre = new IntersectionObserver(entries =>{
         projquatre.classList.remove('entreedroite');
      });
 });
+const observercinq = new IntersectionObserver(entries =>{
+
+    entries.forEach(entry => {
+
+        const projcinq = entry.target.querySelector('#projetmaquetteun');
+
+        if (entry.isIntersecting){
+         projcinq.classList.add('entreegauche')
+         return;
+        }
+        
+        projcinq.classList.remove('entreegauche');
+     });
+});
+const observersix = new IntersectionObserver(entries =>{
+
+    entries.forEach(entry => {
+
+        const projsix = entry.target.querySelector('#projetmaquettedeux');
+
+        if (entry.isIntersecting){
+         projsix.classList.add('entreedroite')
+         return;
+        }
+        
+        projsix.classList.remove('entreedroite');
+     });
+});
+const observersept = new IntersectionObserver(entries =>{
+
+    entries.forEach(entry => {
+
+        const projsept = entry.target.querySelector('#projetmaquettetrois');
+
+        if (entry.isIntersecting){
+         projsept.classList.add('entreegauche')
+         return;
+        }
+        
+        projsept.classList.remove('entreegauche');
+     });
+});
+const observerhuit = new IntersectionObserver(entries =>{
+
+    entries.forEach(entry => {
+
+        const projhuit = entry.target.querySelector('#projetmaquettequatre');
+
+        if (entry.isIntersecting){
+         projhuit.classList.add('entreedroite')
+         return;
+        }
+        
+        projhuit.classList.remove('entreedroite');
+     });
+});
+const observerneuf = new IntersectionObserver(entries =>{
+
+    entries.forEach(entry => {
+
+        const projneuf = entry.target.querySelector('#projetmaquettecinq');
+
+        if (entry.isIntersecting){
+         projneuf.classList.add('entreegauche')
+         return;
+        }
+        
+        projneuf.classList.remove('entreegauche');
+     });
+});
 
 
 
@@ -78,3 +148,8 @@ observer.observe(document.querySelector('#contprojun'));
 observerdeux.observe(document.querySelector('#contprojdeux'));
 observertrois.observe(document.querySelector('#contprojtrois'));
 observerquatre.observe(document.querySelector('#contprojquatre'));
+observercinq.observe(document.querySelector('#contprojmaquetteun'));
+observersix.observe(document.querySelector('#contprojmaquettedeux'));
+observersept.observe(document.querySelector('#contprojmaquettetrois'));
+observerhuit.observe(document.querySelector('#contprojmaquettequatre'));
+observerneuf.observe(document.querySelector('#contprojmaquettecinq'));
